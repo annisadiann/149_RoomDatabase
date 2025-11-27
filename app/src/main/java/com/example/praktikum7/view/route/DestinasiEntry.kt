e1fd305febecda2f0016022e0639f763010d2158
annisadiann <annisadian0412@gmail.com>
2025-11-27 21:21:58 +0700

    menambahkan file DestinasiEntry.kt beserta isi nya

---
+++ app/src/main/java/com/example/praktikum7/view/route/DestinasiEntry.kt
@@ -0,0 +1,9 @@
+package com.example.praktikum7.view.route
+
+import com.example.praktikum7.R
+import com.example.praktikum7.view.route.DestinasiNavigasi
+
+object DestinasiEntry: DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
