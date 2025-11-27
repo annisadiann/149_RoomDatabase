5c477d9c1807b0aa1c8f6d474ff4a53fd39c1357
annisadiann <annisadian0412@gmail.com>
2025-11-27 17:22:31 +0700

    menambahkan class DatabaseSiswa sebagai RoomDatabase dengan entity Siswa

---
+++ app/src/main/java/com/example/praktikum7/room/DatabaseSiswa.kt
@@ -0,0 +1,15 @@
+package com.example.praktikum7.room
+
+import android.content.Context
+import android.provider.CalendarContract
+import androidx.compose.runtime.IntState
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Siswa::class], version = 1, exportSchema = false)
+abstract class DatabaseSiswa : RoomDatabase() {
+    abstract fun siswaDao() : SiswaDao
+
+
+}
