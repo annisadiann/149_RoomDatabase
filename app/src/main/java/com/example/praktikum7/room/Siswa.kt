97a2750692fc114f9a8e76a777f14c42e64c9348
annisadiann <annisadian0412@gmail.com>
2025-11-27 17:15:37 +0700

    menambahkan kolom nama, alamat, dan telpon pada entity Siswa

--- app/src/main/java/com/example/praktikum7/room/Siswa.kt
+++ app/src/main/java/com/example/praktikum7/room/Siswa.kt
@@ -7,5 +7,7 @@ import androidx.room.PrimaryKey
 data class Siswa(
     @PrimaryKey(autoGenerate = true)
     val id : Int = 0,
-
+    val nama : String,
+    val alamat : String,
+    val telpon : String
 )
