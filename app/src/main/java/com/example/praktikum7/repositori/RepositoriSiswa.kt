3112ed8b409c582dfc737bbf9f51cb943c254ae5
annisadiann <annisadian0412@gmail.com>
2025-11-27 17:17:34 +0700

    menambahkan interface RepositoriSiswa dengan fungsi dasar CRUD

---
+++ app/src/main/java/com/example/praktikum7/repositori/RepositoriSiswa.kt
@@ -0,0 +1,13 @@
+package com.example.praktikum7.repositori
+
+import com.example.praktikum7.room.Siswa
+import com.example.praktikum7.room.SiswaDao
+import kotlinx.coroutines.flow.Flow
+
+interface RepositoriSiswa {
+    fun getAllSiswaStream(): Flow<List<Siswa>>
+    suspend fun insertSiswa(siswa: Siswa)
+}
+
+
+}
