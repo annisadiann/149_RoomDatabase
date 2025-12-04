07c24fc41deb15e64edc97e43ef29fcf1de569c4
annisadiann <annisadian0412@gmail.com>
2025-12-04 16:29:20 +0700

    Mengoverride fungsi deleteSiswa agar menggunakan delete milik siswaDao

--- app/src/main/java/com/example/praktikum7/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/praktikum7/repositori/RepositoriSiswa.kt
@@ -16,6 +16,5 @@ class OfflineRepositoriSiswa(
 ): RepositoriSiswa {
     override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao.getAllSiswa()
     override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
-
-    override suspend fun deleteSiswa(siswa: Siswa) = siswaDao.delete(siswa)
+    override fun getSiswaStream(id: Int): Flow<Siswa> = siswaDao.getSiswa(id)
 }
