3e2a7b2ea7f7847b92dcf8a71b6ac30477177c29
annisadiann <annisadian0412@gmail.com>
2025-12-04 20:08:21 +0700

    Menambahkan file DestinasiEditSiswa.kt beserta isinya

--- app/src/main/java/com/example/praktikum7/view/route/DestinasiEditSiswa.kt
+++ app/src/main/java/com/example/praktikum7/view/route/DestinasiEditSiswa.kt
@@ -1,4 +1,9 @@
 package com.example.praktikum7.view.route
 
-object DestinasiEditSiswa {
+import com.example.praktikum7.R
+object DestinasiEditSiswa: DestinasiNavigasi {
+    override val route = "item_edit"
+    override val titleRes =  R.string.edit_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
 }
